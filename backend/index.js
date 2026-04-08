53a45ae5d84e9c0338b5937c23927d3dcf70d9ef
Ayush Kumar Thakur <ayush524425@gmail.com>
2026-04-08 14:36:19 +0545

    first commit

--- backend/index.js
+++ backend/index.js
@@ -15,7 +15,7 @@ const propertyRoutes = require("./modules/property/property.routes");
 const app = express();
 
 // Middleware setup
-app.use(cors({ origin: "https://realestateportal.vercel.app/", credentials: true }));
+app.use(cors({ origin: "https://realestateportal.vercel.app", credentials: true }));
 app.use(express.json());
 app.use(cookieParser());
 
