1b516843c84f946d5680f138bdc73a8c8cdeb4fc
Ayush Kumar Thakur <ayush524425@gmail.com>
2026-04-08 18:19:14 +0545

    first commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,27 +1,48 @@
-import React, { useEffect } from 'react'
-import { ToastContainer } from 'react-toastify';
-import { useAuth } from './contexts/AuthContext';
-import AuthPage from './pages/AuthPage'
-import Dashboard from './pages/Dashboard'
-import DashboardSkeleton from './utils/DashboardSkeleton';
+import { Routes, Route } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import AuthPage from "./pages/AuthPage";
+import Dashboard from "./pages/Dashboard";
+import DashboardSkeleton from "./utils/DashboardSkeleton";
+import { useAuth } from "./contexts/AuthContext";
+import ProtectedRoute from "./routes/ProtectedRoute";
+import PublicRoute from "./routes/PublicRoute";
 
 function App() {
-  const { user, loading } = useAuth();
+  const { loading } = useAuth();
 
   if (loading) {
     return (
       <div className="min-h-screen w-full flex flex-col p-4">
         <DashboardSkeleton />
       </div>
-    )
+    );
   }
 
   return (
     <>
       <ToastContainer position="top-center" autoClose={3000} hideProgressBar={false} newestOnTop={false} closeOnClick rtl={false} pauseOnFocusLoss draggable pauseOnHover theme="dark" />
-      {user ? <Dashboard /> : <AuthPage />}
+
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <ProtectedRoute>
+              <Dashboard />
+            </ProtectedRoute>
+          }
+        />
+
+        <Route
+          path="/auth"
+          element={
+            <PublicRoute>
+              <AuthPage />
+            </PublicRoute>
+          }
+        />
+      </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
